ab06872d7c46e127295f39d0c251192774f439ce
jack <jack668@qq.com>
2020-05-15 15:59:57 +0800

    1

--- test/db_test.go
+++ test/db_test.go
@@ -1,13 +1,14 @@
 package test
 
 import (
-	"github.com/EDDYCJY/go-gin-example/models"
+	"github.com/EDDYCJY/go-gin-example/service/order_service"
 	"testing"
 )
 
 func TestDBFind(t *testing.T) {
-	user := models.User{
-		UserId: 1,
-	}
-	user.First()
+	//user := models.User{
+	//	UserId: 1,
+	//}
+	//user.First()
+	order_service.Refund(13)
 }
